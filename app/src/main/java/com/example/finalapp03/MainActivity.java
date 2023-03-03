98d4cc013ef2272dbeb96f9f85f8cdb0f4ed6820
Dragon Black <mdbetico.96@gmail.com>
2023-03-03 22:35:33 +0000

    first commit
    IconoApp, Splash, NavigatorDrawler

---
+++ app/src/main/java/com/example/finalapp03/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.finalapp03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ViewFlipper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constante Contenido Actividad
+    private final static int CONT_ACTIVIDAD = 0;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView nav;
+    private ViewFlipper vf;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Componente ViewFlipper
+        vf = (ViewFlipper)findViewById(R.id.vf);
+        vf.setDisplayedChild(CONT_ACTIVIDAD);
+
+        // Componente NavigationDrawer
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nav = (NavigationView) findViewById(R.id.nav_view);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent sendIntent;
+                switch (item.getItemId()) {
+                    case R.id.nav_item_one:
+                        // Se inicia Actividad 1
+                        sendIntent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(sendIntent);
+                        break;
+                    case R.id.nav_item_two:
+                        // Se inicia Actividad 2
+                        sendIntent = new Intent(MainActivity.this, Activity2.class);
+                        startActivity(sendIntent);
+                        break;
+                    case R.id.nav_item_three:
+                        // Se inicia Actividad 3
+                        sendIntent = new Intent(MainActivity.this, Activity3.class);
+                        startActivity(sendIntent);
+                        break;
+                    case R.id.nav_item_four:
+                        // Se inicia Actividad 4
+                        sendIntent = new Intent(MainActivity.this, Activity4.class);
+                        startActivity(sendIntent);
+                        break;
+                }
+                // Close the navigation drawer when an item is selected
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/finalapp03/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.finalapp03;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    // Constante de tiempo de duración para el Splash Screen
+    final static int TIEMPO = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_background);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, TIEMPO);
+    }
+}
